05b59c8fb734857dd4a4a228edfaadd1b8b052b3
Youssef Elkomy <youssefelkomy500@gmail.com>
2026-01-22 12:59:32 +0200

    feat: basket repository

---
+++ Basket.Domain/Repositories/IBasketRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Basket.Domain.Repositories;
+
+public interface IBasketRepository
+{
+    public Task<Entities.Basket?> Get(string id);
+    public Task<Entities.Basket> Update(Entities.Basket updatedBasket);
+    public Task Delete(string id);
+}
---
+++ Basket.Infrastructure/Repositories/BasketRepository.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+using Basket.Domain.Repositories;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Basket.Infrastructure.Repositories;
+
+public sealed class BasketRepository(IDistributedCache database) : IBasketRepository
+{
+    
+    public async Task<Domain.Entities.Basket?> Get(string id)
+    {
+        var data = await database.GetStringAsync(id);
+        
+        return data is null ? null : JsonSerializer.Deserialize<Domain.Entities.Basket>(data);
+    }
+
+    public async Task<Domain.Entities.Basket> Update(Domain.Entities.Basket updatedBasket)
+    {
+        var data = JsonSerializer.Serialize(updatedBasket);
+        
+        await database.SetStringAsync(updatedBasket.Id, data);
+        
+        return updatedBasket;
+    }
+
+    public async Task Delete(string id)
+    {
+        await database.RemoveAsync(id);
+    }
+}
