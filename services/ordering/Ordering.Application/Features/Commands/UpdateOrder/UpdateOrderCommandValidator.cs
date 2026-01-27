5e59a029b832b2f2e4de136800b7868f7052c69f
Youssef Elkomy <youssefelkomy500@gmail.com>
2026-01-27 12:17:17 +0200

    feat: UpdateOrderCommandValidator and ValidationBehavior

---
+++ services/ordering/Ordering.Application/Features/Commands/UpdateOrder/UpdateOrderCommandValidator.cs
@@ -0,0 +1,54 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Features.Commands.UpdateOrder;
+
+public sealed class UpdateOrderCommandValidator
+    : AbstractValidator<UpdateOrderCommand>
+{
+    private const int NameMaxLength = 50;
+    private const int ZipCodeMaxLength = 20;
+    private const int CvvMinLength = 3;
+    private const int CvvMaxLength = 4;
+    
+    public UpdateOrderCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty();
+
+        When(x => x.UserName is not null, () =>
+        {
+            RuleFor(x => x.UserName)
+                .NotEmpty()
+                .MaximumLength(NameMaxLength);
+        });
+
+        When(x => x.FirstName is not null, () =>
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty()
+                .MaximumLength(NameMaxLength);
+        });
+
+        When(x => x.LastName is not null, () =>
+        {
+            RuleFor(x => x.LastName)
+                .NotEmpty()
+                .MaximumLength(NameMaxLength);
+        });
+
+        When(x => x.ZipCode is not null, () =>
+        {
+            RuleFor(x => x.ZipCode)
+                .MaximumLength(ZipCodeMaxLength);
+        });
+
+        When(x => x.Cvv is not null, () =>
+        {
+            RuleFor(x => x.Cvv)
+                .Length(
+                    CvvMinLength,
+                    CvvMaxLength
+                );
+        });
+    }
+}
